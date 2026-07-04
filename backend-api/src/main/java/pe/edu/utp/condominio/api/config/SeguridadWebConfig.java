2dcd810aa7e428343bdad4e93318d60b86f632e0
Diego <dgarciaespinoza13@gmail.com>
2026-07-03 22:25:49 -0500

    refactor(config): eliminar dependencia inline de HttpMethod

--- backend-api/src/main/java/pe/edu/utp/condominio/api/config/SeguridadWebConfig.java
+++ backend-api/src/main/java/pe/edu/utp/condominio/api/config/SeguridadWebConfig.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -41,7 +42,7 @@ public class SeguridadWebConfig {
                                 "/webjars/**",
                                 "/error")
                         .permitAll()
-                        .requestMatchers(org.springframework.http.HttpMethod.POST, "/api/finanzas/pagos")
+                        .requestMatchers(HttpMethod.POST, "/api/finanzas/pagos")
                         .hasAnyRole("ADMINISTRADOR", "RESIDENTE")
                         .requestMatchers("/api/usuarios/**", "/api/condominios/**", "/api/unidades/**",
                                 "/api/areas-comunes/**", "/api/reservas-areas/**", "/api/incidencias/**",
